923017efcc73894f1c483c273695543677a6dad8
juanfran <juanfranciscorojascampo5@gmail.com>
2023-05-23 14:48:59 +0200

    datatables,jquey,ajax conexiones establecidas mediante CDN para buscar reservas

--- resources/views/admin/plantillabase2.blade.php
+++ resources/views/admin/plantillabase2.blade.php
@@ -12,7 +12,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
-    
+     <!-- referencia a datatable -->
+ @yield('css')
 
     <style>
       .bd-placeholder-img {
@@ -89,8 +90,7 @@
     </style>
 
     
-    <!-- Custom styles for this template -->
-    <link href="sidebars.css" rel="stylesheet">
+   
   </head>
   <body>
     <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
@@ -172,7 +172,7 @@
 </svg>
 
 <main class="d-flex flex-nowrap">
-  <h1 class="visually-hidden">Sidebars examples</h1>
+  <h1 class="visually-hidden">Book&Delivery</h1>
 
   <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
@@ -256,6 +256,10 @@
  
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+<!-- referencia a datatable -->
+@yield('js')
+
+
 </body>
 </body>
 </html>
--- resources/views/reservas/index.blade.php
+++ resources/views/reservas/index.blade.php
@@ -1,5 +1,10 @@
 
 @extends('admin.plantillabase2')
+@section('css')
+<link  href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css" rel="stylesheet"/>
+<link  href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css" rel="stylesheet"/>
+@endsection
+
 @section('content')
 
 <section class="container listado-reversas content-panel">
@@ -10,26 +15,20 @@
     <div class="alert alert-secondary" role="alert">
 <i class="bi bi-info-circle"></i> Aqui podrás gestionar tus reservas   
 </div>
-<a href="{{ route('reserva-create') }}" class="btn btn-primary mt-4"><i class="bi bi-plus-circle-fill"></i></a>
-
-<div class="box-buscador">
-<form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
- </form>
-</div>
+<a href="{{ route('reserva-create') }}" class="btn btn-primary mt-4"><i class="bi bi-plus-circle-fill"></i> Crear Reserva </a>
 
 
-<table class="table table-dark table-striped mt-1">
+
+
+<table  id="reservas_tabla" class="table table-dark table-striped mt-1">
 <thead>
     <tr>
-      
-      <th scope="col">Nombre</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Email</th>
-      <th scope="col">Numero de Personas</th>
-      <th scope="col">Fecha y hora</th>
-      <th scope="col" >Gestionar</th>
+      <th>Nombre</th>
+      <th>Telefono</th>
+      <th>Email</th>
+      <th>Numero de Personas</th>
+      <th>Fecha y hora</th>
+      <th>Gestionar</th>
     </tr>
   </thead>
   <tbody>
@@ -37,42 +36,53 @@
     <tr>
       
       <td>{{  $reserva->nombre }}</td>
-      <td scope="row">{{  $reserva->telefono }}</td>
+      <td>{{  $reserva->telefono }}</td>
       <td>{{  $reserva->email }}</td>
       <td>{{  $reserva->NumPersonas }}</td>
       <td>{{   $reserva->fecha}} a las  {{ $reserva->hora }}</td>
      
        <td>
-       <a href="{{ route('reserva-edit', ['id' => $reserva->id]) }}" class="btn btn-info">Editar</a>
+      
 
       <form action ="{{ route('reserva-destroy', [$reserva->id]) }}" method="post">
-              @method('DELETE')
-              @csrf
+      <a href="{{ route('reserva-edit', ['id' => $reserva->id]) }}" class="btn btn-info">Editar</a>
+      @csrf
+      @method('DELETE')
+             
               <button class="btn btn-danger">Borrar</button>
       </form> 
     
     </td>
     </tr>  
     @endforeach
-    
+</tbody> 
 </table>
-<nav aria-label="Page navigation example">
-  <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>
-   {{ $reservas->links() }}
-    
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-</nav>
-
 
 </section>
+
+@section('js')
+
+<script  src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script  src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script  src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+<script>
+  $(document).ready(function () {
+    $('#reservas_tabla').DataTable({
+      "lengthMenu":[[5,10,50,-1],[5,10,50,"All"]],
+      "language":{
+        "search": "buscar",
+        "lengthMenu": "Mostrar _MENU_ ",
+        "info": "Página _PAGE_ de _PAGES_",
+        "paginate":{
+          "previous":"Anterior",
+          "next":"Siguiente",
+          "first":"Primero",
+          "last":"ultimo"
+        }
+      }
+    });
+});
+</script>
+@endsection
+
 @endsection
