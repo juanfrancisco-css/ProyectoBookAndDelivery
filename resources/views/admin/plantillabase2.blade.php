add1d3d4ee89b851574c7861ec9dfba0ad4a565c
juanfran <juanfranciscorojascampo5@gmail.com>
2023-05-25 13:14:15 +0200

    Establecido nuevas conexiones para el calendario en la platilla del dashboard

--- resources/views/admin/plantillabase2.blade.php
+++ resources/views/admin/plantillabase2.blade.php
@@ -9,6 +9,9 @@
     <meta name="generator" content="Hugo 0.111.3">
     <title>Book&Delivery</title>
 
+     <!-- estilos del  calendario-->
+ <link href="{{ asset('css/calendario.css') }}" rel="stylesheet">
+ 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
@@ -254,7 +257,8 @@
 </main>
 
  
-
+<!-- calendario javascript-->
+<script src="js/calendario.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 <!-- referencia a datatable -->
 @yield('js')
